eaeb0f3e68d7381b407313cde7d043755ea1a7c8
ammarmuhdev <ammarmuhdev@gmail.com>
2025-07-31 02:19:02 -0400

    changedfindEmployeeBySpecialization method to filter instead of find

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -2,6 +2,7 @@ const employees = [
       { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript' },
       { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' },
       { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Java' },
+      { id: 4, name: 'Tony Montana', age: 40, department: 'IT', salary: 70000, specialization:'Javascript' },
       //... More employee records can be added here
     ];
 
@@ -33,9 +34,10 @@ function findEmployeeById(employeeId) {
 }
 
 function findEmployeeBySpecialization(specialization) {
-    const foundEmployee =  employees.find(employee => employee.specialization === specialization)
-    if (foundEmployee) {
-        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specialization}</p>`;
+    const javascriptSpecializedEmployees =  employees.filter(employee => employee.specialization === specialization)
+    if (javascriptSpecializedEmployees.length>0) {
+        javascriptSpecializedEmployeeDisplay = javascriptSpecializedEmployees.map(employee=>`<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('')
+        document.getElementById('employeesDetails').innerHTML = javascriptSpecializedEmployeeDisplay
     }
     else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialization';
